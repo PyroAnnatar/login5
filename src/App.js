c6282b92e24a860c22a5ec3648d60cea12365bea
PyroAnnatar <pyroannatar@gmail.com>
2024-09-27 10:59:58 +0300

    Nitpicking

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => {
 
       <div className="shape relative h-screen bottom-0 w-full -z-10 bg-[#095F59]"></div>
 
-      <div className="min-w-[185px] min-h-[185px] rounded-full absolute bg-[#095F59D6] border-[23px] border-[#02B94B1A] left-36 bottom-[330px]"></div>
+      <div className="min-w-[185px] min-h-[185px] rounded-full absolute bg-[#095F59D6] border-[23px] border-[#02B94B1A] left-36 bottom-[345px]"></div>
 
       <div className="min-w-[278px] min-h-[278px] border-[48px] border-[#02B94B1A] rounded-full bg-[#095F59D6] absolute -bottom-24 -right-16"></div>
     </div>
